f594a73e8151cba2f5272eadb5cb70d64118c55b
Blueskale <mac@macs-MacBook-Air-2.local>
2023-10-11 13:53:35 +0100

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+A module for asynchronous tasks using asyncio.
+"""
+
 import random
 import asyncio
 
@@ -15,3 +19,4 @@ async def wait_random(max_delay=10):
   delay = random.uniform(0, max_delay)
   await asyncio.sleep(delay)
   return delay
+  
